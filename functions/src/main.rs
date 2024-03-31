1379da3b0fa07594a9856308e161062bca806efb
ercanyu <ercan.yildiz@udemy.com>
2024-03-31 14:46:34 +0300

    feat: add examples for functions and expressions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    println!("hello, it's a me, the main function!");
+
+    another_function();
+    function_with_parameters(5);
+    print_labeled_measurement('h', 10);
+
+    /*
+    expressions do not include ending semicolons,
+    if you add a semicolon to the end of an expression,
+    you turn it into a statement, and it will then not return a value
+    */
+    let expression = {
+        let x = 5;
+        x + 1
+    };
+
+    println!("The value of the expression is: {expression}");
+}
+
+fn another_function() {
+    println!("just another function.");
+}
+
+fn function_with_parameters(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(label: char, measurement: i32) {
+    println!("labeled measurement => {measurement}{label}");
 }
