6c62d2628b032d3e77d68fe5ddcfeea7c20b494b
ercanyu <ercan.yildiz@udemy.com>
2024-03-31 13:24:48 +0300

    feat: add constants, mutable variables and shadowing practices

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,22 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
-    println!("Hello, world!");
+    // constants
+    println!("the value of const THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
+
+    // mutable variable
+    let mut x = 5;
+    println!("the value of x is: {x}");
+    x = 6;
+    println!("the value of x is: {x}");
+
+    // shadowing
+    let y = 5;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("the value of y in the inner scope is: {y}");
+    }
+
+    println!("the value of y is: {y}")
 }
